2f070a89b914b624fe0024f4dfccc96154afb902
Dormin <zyxster@gmail.com>
2016-10-20 20:57:31 +0200

    Keyboard test.

--- js/synth.js
+++ js/synth.js
@@ -5,19 +5,31 @@ var Synths = []
 function InitSynths(NumSynths) {
 	for (var i = 0; i < NumSynths; i++) {
 		Synths[i] = {
+			Gate: 0,
 			Freq: 440,
 			Time: 0
 		}
 	}
 }
 
+function SynthNoteOn(Index, Note) {
+	var Synth = Synths[Index]
+	Synth.Gate = 1
+}
+
+function SynthNoteOff(Index) {
+	var Synth = Synths[Index]
+	Synth.Gate = 0
+}
+
 function ProcessSynth(Index, OutputL, OutputR, NumSamples, SampleRate) {
 	var Synth = Synths[Index]
+	var Gate = Synth.Gate
 	var Freq = Synth.Freq
 	var Time = Synth.Time
 
 	for (var i = 0; i < NumSamples; i++) {
-		var Sample = Math.sin(2 * Math.PI * Time)
+		var Sample = Gate * Math.sin(2 * Math.PI * Time)
 		Time += Freq / SampleRate
 		OutputL[i] = Sample
 		OutputR[i] = Sample
