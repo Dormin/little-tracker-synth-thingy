8ea75b7af72763a9fcb29e5fa395ad23182024e0
Dormin <zyxster@gmail.com>
2016-10-22 16:49:08 +0200

    Keyboard input functions take key string as argument instead of key code.

--- js/input.js
+++ js/input.js
@@ -37,8 +37,18 @@ function OnKeyUp(Event) {
 	UpdateKeyState(Event.keyCode, false)
 }
 
-function UpdateKeyState(Key, IsHeld) {
+function UpdateKeyState(KeyCode, IsHeld) {
+	var Key = KeyCodeToString(KeyCode)
 	Input.WasPressed[Key] = IsHeld && !Input.IsHeld[Key]
 	Input.WasReleased[Key] = !IsHeld && Input.IsHeld[Key]
 	Input.IsHeld[Key] = IsHeld
+}
+
+function KeyCodeToString(KeyCode) {
+	// Alpha numeric?
+	if ((KeyCode >= 48 && KeyCode <= 57) || (KeyCode >= 65 && KeyCode <= 90)) {
+		return String.fromCharCode(KeyCode)
+	}
+
+	return ""
 }
