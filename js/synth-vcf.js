935002a5ab6a525090ef21bce4f0ff4adc3a8be8
Dormin <zyxster@gmail.com>
2016-11-07 11:30:50 +0100

    Better sounding filter plus some test sounds.

--- js/synth-vcf.js
+++ js/synth-vcf.js
@@ -5,10 +5,14 @@ var SynthVcf = {
 	Resonance: [],
 	EgInt: [],
 	Output: [],
-	X1: [],
-	X2: [],
-	Y1: [],
-	Y2: []
+	In1: [],
+	In2: [],
+	In3: [],
+	In4: [],
+	Out1: [],
+	Out2: [],
+	Out3: [],
+	Out4: []
 }
 
 function InitSynthVcf() {
@@ -17,49 +21,57 @@ function InitSynthVcf() {
 		SynthVcf.Resonance[Track] = 0
 		SynthVcf.EgInt[Track] = 0
 		SynthVcf.Output[Track] = CreateBuffer(Audio.BufferSize)
-		SynthVcf.X1[Track] = 0
-		SynthVcf.X2[Track] = 0
-		SynthVcf.Y1[Track] = 0
-		SynthVcf.Y2[Track] = 0
+		SynthVcf.In1[Track] = 0
+		SynthVcf.In2[Track] = 0
+		SynthVcf.In3[Track] = 0
+		SynthVcf.In4[Track] = 0
+		SynthVcf.Out1[Track] = 0
+		SynthVcf.Out2[Track] = 0
+		SynthVcf.Out3[Track] = 0
+		SynthVcf.Out4[Track] = 0
 	}
 }
 
 function ProcessSynthVcf(Track, NumSamples) {
 	var SampleRate = Audio.SampleRate
 	var Cutoff = SynthVcf.Cutoff[Track]
-	var Resonance = Math.pow(10, -2 * SynthVcf.Resonance[Track])
+	var Resonance = SynthVcf.Resonance[Track]
 	var EgInt = SynthVcf.EgInt[Track]
 	var Output = SynthVcf.Output[Track]
-	var X1 = SynthVcf.X1[Track]
-	var X2 = SynthVcf.X2[Track]
-	var Y1 = SynthVcf.Y1[Track]
-	var Y2 = SynthVcf.Y2[Track]
+	var In1 = SynthVcf.In1[Track]
+	var In2 = SynthVcf.In2[Track]
+	var In3 = SynthVcf.In3[Track]
+	var In4 = SynthVcf.In4[Track]
+	var Out1 = SynthVcf.Out1[Track]
+	var Out2 = SynthVcf.Out2[Track]
+	var Out3 = SynthVcf.Out3[Track]
+	var Out4 = SynthVcf.Out4[Track]
 	var EgOutput = SynthEg.Output[Track]
 	var VcoOutput = SynthVco.Output[Track]
 
 	for (var i = 0; i < NumSamples; i++) {
-		var Cut = Cutoff + EgInt * EgOutput[i]
-		Cut = 20000 * (Math.pow(2, Cut) - 1) / SampleRate
-		var K = 0.5 * Resonance * Math.sin(Math.PI * Cut)
-		var C1 = 0.5 * (1 - K) / (1 + K)
-		var C2 = (0.5 + C1) * Math.cos(Math.PI * Cut)
-		var C3 = (0.5 + C1 - C2) / 4
-		var A0 = 2 * C3
-		var A1 = 2 * 2 * C3
-		var A2 = 2 * C3
-		var B1 = 2 * -C2
-		var B2 = 2 * C1
-		var In = VcoOutput[i]
-		var Out = A0 * In + A1 * X1 + A2 * X2 - B1 * Y1 - B2 * Y2
-		X2 = X1
-		X1 = In
-		Y2 = Y1
-		Y1 = Out
-		Output[i] = Out
+		var F = (Cutoff + EgInt * EgOutput[i]) * 1.16
+		var Fb = 4 * Resonance * (1.0 - 1.15 * F * F)
+		var Input = VcoOutput[i]
+		Input -= Out4 * Fb
+		Input *= 0.35013 * F * F * F * F
+		Out1 = Input + 0.3 * In1 + (1 - F) * Out1
+		In1 = Input
+		Out2 = Out1 + 0.3 * In2 + (1 - F) * Out2
+		In2 = Out1
+		Out3 = Out2 + 0.3 * In3 + (1 - F) * Out3
+		In3 = Out2
+		Out4 = Out3 + 0.3 * In4 + (1 - F) * Out4
+		In4 = Out3
+		Output[i] = Out4
 	}
 
-	SynthVcf.X1[Track] = X1
-	SynthVcf.X2[Track] = X2
-	SynthVcf.Y1[Track] = Y1
-	SynthVcf.Y2[Track] = Y2
+	SynthVcf.In1[Track] = In1
+	SynthVcf.In2[Track] = In2
+	SynthVcf.In3[Track] = In3
+	SynthVcf.In4[Track] = In4
+	SynthVcf.Out1[Track] = Out1
+	SynthVcf.Out2[Track] = Out2
+	SynthVcf.Out3[Track] = Out3
+	SynthVcf.Out4[Track] = Out4
 }
